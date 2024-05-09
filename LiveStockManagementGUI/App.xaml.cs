d1e5837e20897be0a6743add36e86ec73be3923c
Jung Kim <jung.kim8505@gmail.com>
2024-05-09 14:09:52 +1200

    Add project files.

---
+++ LiveStockManagementGUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LiveStockManagementGUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ LiveStockManagementGUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LiveStockManagementGUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
