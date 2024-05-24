6f8b9214175067c5df6c7fa99876ae10164a344e
Jung Kim <jung.kim8505@gmail.com>
2024-05-24 13:11:39 +1200

    Done Model.cs wool to milk

--- LiveStockManagementGUI/Models/Model.cs
+++ LiveStockManagementGUI/Models/Model.cs
@@ -4,13 +4,14 @@ public class Livestock
 {
     [PrimaryKey, AutoIncrement]
     public int Id { get; set; } //primary Key 
-    public string? Name { get; set; }
+    public virtual string? Name { get; set; }
     public string? Colour { get; set; }
-    public double Milk { get; set; }
+    public virtual double Milk { get; set; }
 
     public double Weight { get; set; }
     public double Cost { get; set; }
    
+
     public override string ToString() // used if there is instance of Livestock class 
     {
         //GetType().Name : either Cow or  Sheep
@@ -20,8 +21,13 @@ public class Livestock
 }
     [Table("Cow")] //Cow from the data table, feature fo mapped, table name
     public class Cow : Livestock
-{
-        public int NumCows { get; set; }
+    {
+
+        public Cow()
+        {
+            Name = "Cow";  // Name 
+        }
+    public int NumCows { get; set; }
         public override string ToString()
         {
             return base.ToString() + $"{this.NumCows}"; // subclass of ToString (up)
@@ -29,9 +35,22 @@ public class Livestock
     }
     [Table("Sheep")]
     public class Sheep : Livestock
-{
+    {
+        public Sheep()
+        {
+            Name = "Sheep";  // Name 속성 설정
+        }
         public int NumSheeps {  get; set; }
-      
+        // Wool property to hold the wool value
+        public double Wool { get; set; }
+
+        // Override the Milk property to return the Wool value
+        public override double Milk
+        {
+            get => Wool;
+            set => Wool = value; // Allow setting Milk to set Wool
+        }
+
     public override string ToString()
         {
             return base.ToString() + $"{this.NumSheeps}";
